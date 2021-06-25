a27a8cf69d7ff513303f10c19ac7b18360cb942a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-06-25 23:29:46 +0530

    Complete React Fundamentals Styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,16 +12,18 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue'}}>small lightblue box</div>
-const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink'}}>medium pink box</div>
-const largeBox = <div className="box box--large" style={{backgroundColor: 'orange'}}>large orange box</div>
+//const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue'}}>small lightblue box</div>
+//const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink'}}>medium pink box</div>
+//const largeBox = <div className="box box--large" style={{backgroundColor: 'orange'}}>large orange box</div>
+
+const Box = ({size, children, styles}) => <div className={`box ${size ? 'box--'+ size : 'box--medium' }`} style={{fontStyle: 'italic', ...styles}}>{children}</div>
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      {<Box size="small" children="small lightblue box" styles={{backgroundColor: 'lightblue'}}/>}
+      {<Box children="medium pink box" styles={{backgroundColor: 'pink'}}/>}
+      {<Box size="large" children="large orange box" styles={{backgroundColor: 'orange'}}/>}
     </div>
   )
 }
